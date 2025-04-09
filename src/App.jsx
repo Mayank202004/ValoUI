694173ed7e03f79bb0ae74aedf7d0fc72d0cb01a
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-04-09 19:55:14 +0530

    Setup theme
    Setup theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1 className="text-5xl text-orange-500 font-bold">Mayank's Frontend</h1>
+    <h1 className="text-5xl text-orange-500 font-bold font-circular-web">Mayank's Frontend</h1>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,32 @@
-// tailwind.config.js
-export default{
-    content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
-    theme: {
-      extend: {
-        fontFamily: {
-            zentry: ['zentry', 'sans-serif'],
-            circular: ['circular-web', 'sans-serif'],
-            general: ['general', 'sans-serif'],
-            robertMedium: ['robert-medium', 'sans-serif'],
-            robertRegular: ['robert-regular', 'sans-serif'],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        zentry: ["zentry", "sans-serif"],
+        general: ["general", "sans-serif"],
+        "circular-web": ["circular-web", "sans-serif"],
+        "robert-medium": ["robert-medium", "sans-serif"],
+        "robert-regular": ["robert-regular", "sans-serif"],
+      },
+      colors: {
+        blue: {
+          50: "#DFDFF0",
+          75: "#dfdff2",
+          100: "#F0F2FA",
+          200: "#010101",
+          300: "#4FB7DD",
+        },
+        violet: {
+          300: "#5724ff",
+        },
+        yellow: {
+          100: "#8e983f",
+          300: "#edff66",
         },
       },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
