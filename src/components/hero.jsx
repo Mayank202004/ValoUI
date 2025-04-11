5c5750b81c66e2b15991acf7e560b88644f0d903
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-04-11 22:36:23 +0530

    Added work on hero
    Added work on hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from 'react'
+import button from './button';
 
 function Hero() {
 
@@ -23,34 +24,46 @@ function Hero() {
 
   return (
     <div className='relative h-dvh w-screen overflow-x-hidden'>
-        <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounted-lg bg-blue-75'></div>
-        <div className='mask-clip-path absolute-center absolute z-50 size-64 courser-pointer overflow-hidden rounded-lg'>
-          <div onClick={handleMiniVideoClick}
-          className='origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100'>
-            <video
-            src= {getVideoSrc((currentIndex % totalVideos) + 1)} 
+        <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounted-lg bg-blue-75'>
+          <div className='mask-clip-path absolute-center absolute z-50 size-64 courser-pointer overflow-hidden rounded-lg'>
+            <div onClick={handleMiniVideoClick}
+            className='origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100'>
+              <video
+              src= {getVideoSrc((currentIndex % totalVideos) + 1)} 
+              ref={nextVideoRef}
+              loop
+              muted
+              id="current-video"
+              className='size-64 origin-center scale-150 object-cover object-center'
+              />
+            </div>
+          </div>
+          <div>
+            <video 
+            src={getVideoSrc(currentIndex)}
             ref={nextVideoRef}
             loop
             muted
-            id="current-video"
-            className='size-64 origin-center scale-150 object-cover object-center'
-            />
-          </div>
-        </div>
-        <video 
-        src={getVideoSrc(currentIndex)}
-        ref={nextVideoRef}
-        loop
-        muted
-        id='next-video'
-        className='absolute-center invisible absolute z-20 size-64 object-cover object-center'/>
+            id='next-video'
+            className='absolute-center invisible absolute z-20 size-64 object-cover object-center'/>
 
-        <video 
-        src={getVideoSrc(currentIndex === totalVideos-1 ? 1 : currentIndex)}
-        autoPlay
-        loop
-        muted
-        className='absolute left-0 top-0 size-full object-cover object-center'/>
+            <video 
+            src={getVideoSrc(currentIndex === totalVideos-1 ? 1 : currentIndex)}
+            autoPlay
+            loop
+            muted
+            className='absolute left-0 top-0 size-full object-cover object-center'
+            onLoadedData={handleVideoLoad}/>
+          </div>
+          <h1 className='special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75'>G<b>a</b>ming</h1>
+          <div className='absolute left-0 top-0 z-40 size-full'>
+            <div className='mt-24 px-5 sm:px-10'>
+              <h1 className='special-font hero-heading text-blue-100'>redefi<b>n</b>e</h1>
+              <p className='mb-5 max-3-64 font-robert-regular text-blue-100'>Enter the Metagame Layer <br />Unleash the Play Economy</p>
+              <button/>
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
